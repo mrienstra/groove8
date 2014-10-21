5d7f9e61196ad1f1c8d081bf920aab6ba8f866af
Erik Burns <erikburns@server>
2014-10-20 18:15:34 -0700

    adding letter producing logic

--- src/script/hex.js
+++ src/script/hex.js
@@ -1,3 +1,28 @@
+// Globals
+var row_1 = ["Db", "Eb", "Gb", "Bb"];
+var row_2 = ["D", "F", "Ab", "B"];
+var row_3 = ["C", "E", "G", "A"];
+
+var num_hex = 8;
+
+function generate_letters(row) {
+  for (i=0; i<num_hex; i++) {
+    var note = row[(i % row.length)];
+    var incidental = note.substring(1,2);
+    var output = '';
+    if (incidental == '') {
+      output = '<h1>'+note+'</h1>';
+    } else {
+      var sup = (incidental == 'b') ? '&#x266d;' : '&#x266f;';
+      output = '<h1 class="incidental">'+note.substring(0,1)+'<sup>'+sup+'</sup></h1>';
+    }
+    console.log(output);
+  }
+}
+
+generate_letters(row_2);
+
+// Show / hide letters
 $('#letters').click(function(){
   $('.key h1').toggle();
 });
